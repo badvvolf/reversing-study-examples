742a1b0b31282335cf0fc01effd38ccab775cbc9
jk <kjkjk1178@gmail.com>
2018-01-09 19:06:28 +0900

    DLL

---
+++ DLL injection_DLL/MyDll.cpp
@@ -0,0 +1,31 @@
+
+#include<windows.h>
+
+HMODULE g_hMod = NULL;
+HANDLE hThread = NULL;
+
+
+void WINAPI my_Function()
+{
+	MessageBoxA(NULL, "hello", "do something weird", 0);
+}
+
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+	g_hMod = (HMODULE)hinstDLL;
+
+	switch (fdwReason)
+	{
+
+	case DLL_PROCESS_ATTACH:
+		OutputDebugStringA("Dll excuted!!");
+
+		my_Function(); 
+		break;
+		
+	}
+
+	return TRUE;
+
+}
